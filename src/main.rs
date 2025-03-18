44484b54bf78d57ed5a2a952ab50ad40b7260c8a
Ryan Shang <50010920+ryqdev@users.noreply.github.com>
2025-03-18 09:51:37 +0800

    chore: format the code

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
- let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
